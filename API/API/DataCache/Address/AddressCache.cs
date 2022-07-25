bc42fee44eb075bd10c1a4250eb8057456cdbcca
NeilVermeulen <neilocv@gmail.com>
2022-07-25 18:37:44 +0200

    Building Address CACHE

---
+++ API/API/DataCache/Address/AddressCache.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Models;
+using DAL.DataAccess;
+
+namespace API.DataCache;
+
+public class AddressCache : IAddressCache
+{
+    DataContext _db;
+    private List<Address> _addresses = new List<Address>();
+    public List<Address> Addresses 
+    {
+        get => ReadAll();
+    }
+
+    private List<Address> ReadAll()
+    {
+        if (!_addresses.Any())
+        {
+            var addresses = (from tb in _db.Addresses where tb.IsDeleted == false select tb).ToList();
+            _addresses = addresses;
+            return _addresses;
+        }
+        else return null;
+
+    }
+
+    public List<Address> All()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Create(Address Address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(Address Address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Address Get(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Address Address)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ API/API/DataCache/Address/IAddressCache.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DataCache;
+
+public interface IAddressCache
+{
+    List<DAL.Models.Address> Addresses { get; set; }
+    int Create(DAL.Models.Address Address);
+    List<DAL.Models.Address> All();
+    DAL.Models.Address Get(int Id);
+    bool Update(DAL.Models.Address Address);
+    bool Delete(DAL.Models.Address Address);
+
+}
